787e17689266bc6e47b0e7e1bf67276e3d67e058
Andrei Gridin <andreigridin918@gmail.com>
2024-02-17 20:17:01 +0500

    Добавьте файлы проекта.

---
+++ assertUsing.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+float travelTime(float d, float s) {
+    assert(s > 0);
+    float time = d * s;
+    return time;
+}
+
+int main()
+{
+    system("color 80");
+    float distance, speed;
+    cout << " Enter distance and speed: " << endl;
+    cout << ' ';
+    cin >> distance;
+    cout << ' ';
+    cin >> speed;
+    cout << " Result time: " << travelTime(distance, speed) << endl;
+    system("pause>nul");
+    return 0;
+}
